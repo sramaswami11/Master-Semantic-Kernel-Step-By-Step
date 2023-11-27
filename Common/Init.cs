fde45a4ec3d80191fdf02fd48f93ad4597e277fb
sramaswami11 <sramaswami11@gmail.com>
2023-11-27 16:11:18 -0500

    added methods to init

--- Common/Init.cs
+++ Common/Init.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
 
 namespace Common;
 
@@ -6,7 +7,7 @@ public static class Init
 {
     const string modelId = "gpt-3.5-turbo";
     #region OPENAI_APIKEY
-    const string myApiKey = "{Your API Key}";
+    const string myApiKey = "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";
     #endregion
 
     public static IKernel Setup()
@@ -18,6 +19,44 @@ public static class Init
              ).Build();
 
     }
+
+    public static IDictionary<string, ISKFunction> ImportPlugins(IKernel kernel,
+                                                                string pluginRoot,
+                                                                string pluginTopDir)
+    {
+        var pluginsDirectory = Path.Combine(System.IO.Directory.GetCurrentDirectory(), pluginRoot);
+        return kernel.ImportSemanticFunctionsFromDirectory(pluginsDirectory, pluginTopDir);
+    }
+
+    public static async Task<KernelResult> GetIntent(IKernel kernel, 
+        IDictionary<string, ISKFunction> plugin)
+    {
+
+        var myContext = new ContextVariables();
+
+       
+        myContext.Set("INPUT", "These pretzels are making me thirsty!");
+
+        var result = await kernel.RunAsync(myContext, plugin["GetIntent"]);
+        return result;
+    }
+
+    public static async Task<KernelResult> GetIntentTemplatized(IKernel kernel, 
+                                            IDictionary<string, ISKFunction> plugin)
+    {
+
+        var variables = new ContextVariables
+        {
+            ["input"] = "Yes",
+            ["history"] = @"Bot: How can I help you?
+User: Admin of our R&D division of my company has sent us an invite for the Annual Holiday party.",
+            ["options"] = "SendEmail, ReadEmail, SendMeeting, RsvpToMeeting, SendChat"
+        };
+
+        var result = await kernel.RunAsync(variables, plugin["GetIntentTemp"]);
+        return result;
+    }
+
 
 
 }
--- Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345a1db229b0fe35fbcb4d571e7ad134290dfb56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
