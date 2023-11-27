1274484018025705ac90e14fd2d3c572209325d6
sramaswami11 <sramaswami11@gmail.com>
2023-11-27 16:14:14 -0500

    new project for part2 of the series

---
+++ SK-Master2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel;
+
+var kernel = Common.Init.Setup();
+
+var myPlugin = Common.Init.ImportPlugins(kernel, "plugins", "OrchestratorPlugin");
+var result = await Common.Init.GetIntent(kernel, myPlugin);
+Console.WriteLine(result);
+
+result = await Common.Init.GetIntentTemplatized(kernel, myPlugin);
+
+Console.WriteLine(result);
---
+++ SK-Master2/Readme.md
@@ -0,0 +1,3 @@
+﻿This project involves creating AI plugins in Semantic Kernel
+Refer to the URL:
+https://learn.microsoft.com/en-us/semantic-kernel/ai-orchestration/plugins/?tabs=Csharp
