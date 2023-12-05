a7ad892c22297d656031d9a39dc2bbb96a5b7f7d
Siva Ramaswami <sramaswami11@gmail.com>
2023-12-05 11:08:05 -0500

    New project added for Native functions

---
+++ SK-Master3/Plugins/WeatherPlugin/Weather.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugins.WeatherPlugin;
+
+public  class Weather
+{
+    private  Dictionary<string, double> cityTemperatures = new Dictionary<string, double>
+    {
+        {"Atlanta", 53},
+        {"Boston", 37},
+        {"Chicago", 32},
+        {"Detroit", 32},
+        {"El Paso", 59},
+        {"Frankfurt", 41},
+        {"Green Bay", 26},
+        {"Houston", 60},
+        {"Indianapolis", 34},
+        {"Jacksonville", 66}
+    };
+
+    [SKFunction, Description("Convert Fahrenheit to Celsius")]
+    public double ToCelsius([Description("The number to convert to Celsius")] double input)
+    {
+        var value =  (input - 32) * 5/9;
+        return value;
+    }
+
+    [SKFunction, Description("Get temperature difference between two cities")]
+    public double GetTemperatureDifference(string city1, string city2)
+    {
+        if (cityTemperatures.ContainsKey(city1) && cityTemperatures.ContainsKey(city2))
+        {
+            double temperatureCity1 = cityTemperatures[city1];
+            double temperatureCity2 = cityTemperatures[city2];
+            return Math.Abs(temperatureCity1 - temperatureCity2);
+        }
+        else
+        {
+            // Handle cases where city names are not found in the dictionary
+            Console.WriteLine("Invalid city names.");
+            return -1; // Use -1 to indicate an error or invalid input
+        }
+    }
+
+
+}
---
+++ SK-Master3/Program.cs
@@ -0,0 +1,27 @@
+﻿using HandlebarsDotNet;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+
+var kernel = Common.Init.Setup();
+//var result = await kernel.RunAsync("95", weatherPlugin["ToCelsius"]);
+
+// Import the Math Plugin
+var weatherPlugin = kernel.ImportFunctions(new Plugins.WeatherPlugin.Weather(), "WeatherPlugin");
+// Make a request that runs the Sqrt function
+var result = await Common.Init.RunKernelAsync(kernel,"95", weatherPlugin,"ToCelsius");
+Console.WriteLine(result.GetValue<double>());
+
+
+
+//Create the context variables for the Temperature Difference function
+var contextVariables = new ContextVariables
+{
+    ["city1"] = "Atlanta",
+    ["city2"] = "Houston"
+};
+result = await Common.Init.RunKernelAsync(kernel, contextVariables, weatherPlugin, "GetTemperatureDifference");
+Console.WriteLine(result.GetValue<double>());
+
+//Make a request that runs the GetTemperatureDifference function
+//result = await kernel.RunAsync(contextVariables, weatherPlugin["GetTemperatureDifference"]);
+//Console.WriteLine(result);
